e022b2cf69a8e53e42cf4889a68041dc5a3d65ef
Ivan Velev <vanecat@gmail.com>
2023-08-10 16:09:12 -0700

    added monitor init and status check

--- aliases.sh
+++ aliases.sh
@@ -11,11 +11,57 @@ fi
 . $HY_MONITORING_HOME/edit-ini.sh
 
 hyphae-monitor-init() {
+  cd $HY_MONITORING_HOME
   hyphae-edit-ini monitor.ini 'port,secret' 'cmd,cmd_label,cmd_expected_ouput'
+  if [ "$(ps aux | grep monitor-server.py | grep -v | wc -l | tr -d ' ')" = "0" ]; then
+    echo 'Monitor Server is starting on port ' $(grep port monitor.ini | sed -e 's/port=//' -e 's/ //g' )
+    nohup python3 monitor-server.py 2>$HY_MONITORING_HOME/monitor.error 1>$HY_MONITORING_HOME/monitor.log &
+    echo ' ...started'
+  else
+    echo 'Monitor Server is running on port ' $(grep port monitor.ini | sed -e 's/port=//' -e 's/ //g' )
+  fi
+}
+
+hyphae-monitor-status() {
+  if [ ! -f monitor.ini ]; then
+    echo 'Missing monitor.ini'
+    echo 'Please run hyphae-monitor-init command'
+    return 1
+  fi
+  if [ "$(ps aux | grep monitor-server.py | grep -v | wc -l | tr -d ' ')" = "0" ]; then
+    echo 'Monitor Server is NOT running'
+    return 1
+  else
+    echo 'Monitor Server is running on port ' $(grep port monitor.ini | sed -e 's/port=//' -e 's/ //g' )
+  fi
+  echo 'Monitor is check the following commands, with label and expected output:'
+  $(grep '^cmd' monitor.ini | sed -E -e 's/^cmd_//' )
 }
 
 hyphae-monitor-all-init() {
   hyphae-edit-ini monitor-all.ini 'port,from_email,secret' 'url,url_id,url_name,url_secret,alert_email'
+  if [ "$(ps aux | grep monitor-all-server.py | grep -v | wc -l | tr -d ' ')" = "0" ]; then
+    echo 'Monitor ALL Server is running on port ' $(grep port monitor-all.ini | sed -e 's/port=//' -e 's/ //g' )
+    nohup python3 monitor-all-server.py 2>$HY_MONITORING_HOME/monitor.error 1>$HY_MONITORING_HOME/monitor.log &
+  else
+    echo 'Monitor ALL Server is running on port ' $(grep port monitor-all.ini | sed -e 's/port=//' -e 's/ //g' )
+  fi
+}
+
+hyphae-monitor-all-status() {
+  if [ ! -f monitor-all.ini ]; then
+    echo 'Missing monitor-all.ini'
+    echo 'Please run hyphae-monitor-all-init command'
+    return 1
+  fi
+  if [ "$(ps aux | grep monitor-all-server.py | grep -v | wc -l | tr -d ' ')" = "0" ]; then
+    echo 'Monitor ALL Server is NOT running'
+    return 1
+  else
+    echo 'Monitor ALL Server is running on port ' $(grep port monitor-all.ini | sed -e 's/port=//' -e 's/ //g' )
+  fi
+  echo 'Monitor is monitoring the following URLs:'
+  $(grep -E '^(url|alert)' monitor-all.ini | sed -E -e 's/^url_//' )
 }
 
 hyphae-self-update() {
